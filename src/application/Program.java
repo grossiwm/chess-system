1c7eea9b4ce1ad9d42bb9e19b71f868c85450299
Gabriel Rossi Lopes <rossi.gabriel@outlook.com>
2019-06-09 18:02:36 -0300

    Created Position class.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import gameboard.Position;
+
 public class Program {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		Position position = new Position(5, 6);
+		System.out.println(position);
 	}
 }
